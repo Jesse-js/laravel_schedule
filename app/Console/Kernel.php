28d4728abeb349e9564c45ff0d3efb2785398e43
Jesse-js <jesse-jjs@hotmail.com>
2024-06-22 23:21:34 -0300

    feat: adds new command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('app:test-command')->everyMinute();
         $schedule->command('app:my-test')->before(function () {
             Log::info("Before command in console.php");
         })->after(function () {
